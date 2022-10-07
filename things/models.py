4304fc7aef50e5573cd9894f0badd855d52d67a6
William Lingard <William.Lingard@kcl.ac.uk>
2022-10-07 21:17:28 +0100

    Max-length change to model

--- things/models.py
+++ things/models.py
@@ -3,6 +3,10 @@ from django.db.models import Model
 
 # Create your models here.
 class Thing(Model):
-    name = models.CharField()
-    description = models.CharField()
+    name = models.CharField(
+        max_length=30
+    )
+    description = models.CharField(
+        max_length=520
+    )
     quantity = models.IntegerField()
